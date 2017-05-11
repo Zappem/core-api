4d0d1947bc9f18dc45b63f0b4a5c8168d5fdf600
Dan Johnson <dan@danj.eu>
2017-05-12 00:01:45 +0100

    Removed old code

--- api/services/InstanceService.js
+++ api/services/InstanceService.js
@@ -1,16 +1,9 @@
 var Instance = require('../models/InstanceModel.js');
-var Services = {
-    Exceptions: require('./ExceptionService.js')
-};
 
 module.exports = {
 
     create: function (data) {
         return Instance.create(data).save();
-        // return Promise.all([
-        //     ,
-        //     Services.Exceptions.incrementTimes(data.error_id)
-        // ]);
     }
 
 };
