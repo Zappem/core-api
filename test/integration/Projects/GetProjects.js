c748e99512599a9fdbc5c13f122e3e9bcfe20147
Dan Johnson <dan@danj.eu>
2017-05-15 22:23:45 +0100

    Added test cases for the /projects endpoint

--- test/integration/Projects/GetProjects.js
+++ test/integration/Projects/GetProjects.js
@@ -2,11 +2,11 @@ var chai = require('../../TestCase.js');
 
 var Project = require('../../../api/models/ProjectModel.js');
 
-describe('/exceptions endpoint', function() {
+describe('/projects endpoint', function() {
 
-    var endpoint = "/exceptions";
+    var endpoint = "/projects";
 
-    describe('Getting all exceptions', function() {
+    describe('Getting all projects', function() {
 
         it('should return an empty array when none saved', function(done){
             chai.request(chai.server)
@@ -16,6 +16,18 @@ describe('/exceptions endpoint', function() {
                     chai.expect(res.body).to.have.lengthOf(0);
                     done();
                 });
+        });
+
+        it('should return a populated array when some are saved', function(done){
+            Project.create({name: "test"}).save().then(function() {
+                chai.request(chai.server)
+                    .get(endpoint)
+                    .end(function (err, res) {
+                        chai.expect(res).to.be.array;
+                        chai.expect(res.body).to.have.lengthOf(1);
+                        done();
+                    });
+            });
         });
 
     });
