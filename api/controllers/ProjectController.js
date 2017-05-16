18a61bf46cc9a0f33c40f257f62f8861e331c86d
Dan Johnson <dan@danj.eu>
2017-05-16 07:15:20 +0100

    Fixed a bug where the ProjectController failed to load the ProjectService

--- api/controllers/ProjectController.js
+++ api/controllers/ProjectController.js
@@ -1,25 +1,27 @@
+var ProjectService = require('../services/ProjectService.js');
+
 module.exports = {
 
     showAll: function(req, res){
-        req.app.services.Projects.all().then(function(all){
+        ProjectService.all().then(function(all){
             res.json(all);
         });
     },
 
     findById: function(req, res){
-        req.app.services.Projects.findById(req.params.id).then(function(proj){
+        ProjectService.findById(req.params.id).then(function(proj){
             res.json(proj);
         });
     },
 
     updateById: function(req, res){
-        req.app.services.Projects.updateById(req.params.id, req.body).then(function(proj){
+        ProjectService.updateById(req.params.id, req.body).then(function(proj){
             res.json(proj);
         });
     },
 
     createNew: function(req, res){
-        req.app.services.Projects.create(req.body).then(function(proj){
+        ProjectService.create(req.body).then(function(proj){
             res.json(proj)
         });
     }
